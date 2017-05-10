edfd5665d07481f4e9f07f675687a73cc8f2f270
Feras Khoursheed <ferasman@gmail.com>
2017-05-11 00:48:07 +0400

    Updated to FuseBox 2.0.0

--- fuse.js
+++ fuse.js
@@ -28,32 +28,3 @@ fuse.bundle("app")
   .instructions("> main.ts");
 
 fuse.run();
-
-//const fuseBox = fsbx.FuseBox.init({
-//  cache: false,
-//  homeDir: "src/",
-//  sourceMap: {
-//    bundleReference: "app.js.map",
-//    outFile: "./www/app.js.map"
-//  },
-//  outFile: "./www/app.js",
-//  plugins: [
-//    [
-//      fsbx.SassPlugin({outputStyle: "compressed"}),
-//      fsbx.CSSPlugin()
-//    ],
-//    [
-//      fsbx.CSSResourcePlugin({
-//        inline: true
-//      }), fsbx.CSSPlugin()
-//    ],
-//
-//    fsbx.TypeScriptHelpers(),
-//    fsbx.JSONPlugin(),
-//    fsbx.HTMLPlugin({useDefault: false})
-//  ]
-//});
-//
-//fuseBox.devServer(">main.ts", {
-//  port: 8100
-//});
